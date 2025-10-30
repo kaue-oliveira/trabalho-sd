c6ec2715027cc429a4f567c2c09600c0b7b4fdec
kaue-oliveira <kaueolivsil@gmail.com>
2025-10-29 21:33:33 -0300

    feat: adiciona configuração inicial do Spring Boot

---
+++ cafe-agents/src/main/java/br/ufla/sd/cafe_agents/CafeAgentsApplication.java
@@ -0,0 +1,13 @@
+package br.ufla.sd.cafe_agents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeAgentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeAgentsApplication.class, args);
+	}
+
+}
